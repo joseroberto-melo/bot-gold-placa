f571f7c92a0e3752e82cb145f9c33c126391f246
joseroberto-melo <jose0604roberto@gmail.com>
2025-03-26 13:52:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -38,3 +38,7 @@ def home():
 def disparar():
     enviar_mensagem()
     return "Mensagem enviada pro Discord!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
